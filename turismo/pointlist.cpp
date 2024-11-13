1c61eac719f4b85df11b0fde45e80f86c0e0e147
Fwolagos <aantonypv@gmail.com>
2024-11-13 17:36:49 -0600

    Feat: the PointList class was create

---
+++ turismo/pointlist.cpp
@@ -0,0 +1,59 @@
+#include "pointlist.h"
+
+PointList::PointList() {
+	this->head = nullptr;
+}
+PointList::~PointList() {}
+
+void PointList::addPoint(string name, int x, int y) {
+	PointNode* newNode = new PointNode(name, x, y);
+	if (head == nullptr) {
+		head = newNode;
+	}
+	else {
+		PointNode* current = head;
+		while (current->getNext() != nullptr) {
+			current = current->getNext();
+		}
+		current->setNext(newNode);
+		newNode->setPrev(current);
+	}
+}
+PointNode* PointList::searchPoint(int position[2]) {
+	PointNode* temp = head;
+	while (temp->getNext() != nullptr && temp->getX() != position[0] && temp->getY() != position[1]) {
+		temp = temp->getNext();
+	}
+	if (temp->getNext() != nullptr && temp->getX() != position[0] && temp->getY() != position[1]) {
+		return nullptr;
+	}
+	else {
+		return temp;
+	}
+}
+void PointList::delPoint(PointNode* point) {
+	PointNode* next = new PointNode();
+	PointNode* prev = new PointNode();
+	prev = point->getPrev();
+	next = point->getNext();
+	prev->setNext(next);
+	next->setPrev(prev);
+	delete point;
+}
+void editPointName(PointNode* point, string name) {
+	point->setName(name);
+}
+void editPointPosition(PointNode* point, int position[2]) {
+	point->setX(position[0]);
+	point->setY(position[1]);
+}
+
+
+
+
+
+
+
+
+
+
---
+++ turismo/pointlist.h
@@ -0,0 +1,23 @@
+#pragma once
+#ifndef POINTLIST_H
+#define POINTLIST_H
+#include "pointnode.h"
+#include <iostream>
+
+class PointList
+{
+private:
+	PointNode* head;
+public:
+	PointList();
+	~PointList();
+
+	void addPoint(string, int, int);
+	PointNode* searchPoint(int[2]);
+	void delPoint(PointNode*);
+	void editPointName(PointNode*, string);
+	void editPointPosition(PointNode*, int[2]);
+
+
+};
+#endif
